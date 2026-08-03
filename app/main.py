cdd9a0c092fd02a61f3d3dcdb11c5876da54b51a
Omveer Singh <omveer.singh@blitzscale.co>
2026-08-03 13:18:48 +0530

    Initial FastAPI application

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import socket
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Hello from Kubernetes",
+        "hostname": socket.gethostname()
+    }
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+
+@app.get("/version")
+def version():
+    return {
+        "version": "1.0.0",
+        "environment": os.getenv("ENV", "local")
+    }
